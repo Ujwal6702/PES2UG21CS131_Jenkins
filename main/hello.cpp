a518f3faa51608b8626ef608166c80fc1a4da34a
Ujwal <ujwalujwalc@gmail.com>
2024-03-12 10:16:29 +0530

    triggering

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,7 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "My name is C Ujwal and my SRN is PES2UG21CS131\n";
-  cout << "This is for webhook\n";
   cout << "Webhook successfully enabled\n";
   return 0;
 }
