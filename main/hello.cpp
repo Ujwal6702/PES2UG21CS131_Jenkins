df9d9d3821d247684f11965fe8a68a5dcc424528
Ujwal <ujwalujwalc@gmail.com>
2024-03-12 10:06:08 +0530

    mini update

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "My name is C Ujwal and my SRN is PES2UG21CS131\n";
-  cout << "This is for webhooks\n";
+  cout << "This is for webhook\n";
   return 0;
 }
